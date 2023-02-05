d70d4cdd34e758133e8bd8dec75c43e846bf5238
HimanshiVerma <himV113@gmail.com>
2023-02-05 19:51:54 +0530

    Added my project

---
+++ src/app/MyComponent/about-component/about-component.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-about-component',
+  templateUrl: './about-component.component.html',
+  styleUrls: ['./about-component.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class AboutComponentComponent {
+
+}
